8c1a573236311e2b079410385f622d62dbc5fcb7
Sahal2 <hussainsahal2@gmail.com>
2023-01-12 19:27:06 -0800

    Add appointments

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,12 +1,11 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.Appointments;
 import com.example.demo.model.User;
+import com.example.demo.repository.AppointmentsRepository;
 import com.example.demo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,8 +17,16 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private AppointmentsRepository appointmentsRepository;
+
     @GetMapping("users")
     public List < User > getUsers() {
         return this.userRepository.findAll();
+    }
+
+    @GetMapping("appointments/{name}")
+    public List <Appointments> getAppointments(@PathVariable String name) {
+        return this.appointmentsRepository.findByUser_Name(name);
     }
 }
--- src/main/java/com/example/demo/repository/AppointmentsRepository.java
+++ src/main/java/com/example/demo/repository/AppointmentsRepository.java
@@ -4,6 +4,9 @@ import com.example.demo.model.Appointments;
 import com.example.demo.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AppointmentsRepository extends JpaRepository<Appointments, Long> {
+    public List<Appointments> findByUser_Name(final String Name);
 
 }
